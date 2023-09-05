cece79cbb636802bec1dfdd4c2ee47a9d23f8bea
Reterino <nking1505@gmail.com>
2023-09-06 00:17:10 +1000

    working icons

--- setup.mjs
+++ setup.mjs
@@ -1,5 +1,41 @@
-export function setup(ctx) {
-    ctx.patch(Skill, 'addXP').before(function(amount, masteryAction) {
-      return [amount * 2, masteryAction];
-    });
+// pets: { name: String, icon: String, dungeon: String, found: boolean}
+const pets = []
+
+function petIconTick(props) {
+  return {
+    $template: '#pet-icon-tick',
+    img: props.img
   }
+}
+function petIconCross(props) {
+  return {
+    $template: '#pet-icon-cross',
+    img: props.img
+  }
+}
+function generatePetList() {
+  game.dungeons.forEach(dungeon => {
+    let petDungeon = dungeon.pet.pet;
+    let unlocked = game.petManager.isPetUnlocked(petDungeon);
+    pets.push({ name: petDungeon.id, icon: petDungeon.media, dungeon: dungeon.id, found: unlocked })
+  })
+  console.debug(pets);
+  pets.forEach(pet => {
+    console.log(pet);
+    console.log("tutorial-" + pet.dungeon);
+    console.log(document.getElementById("tutorial-" + pet.dungeon))
+    console.log(document.getElementById("tutorial-" + pet.dungeon).nextSibling.nextSibling)
+    if (pet.found) {
+      ui.create(petIconTick({ img: pet.icon }), document.getElementById("tutorial-" + pet.dungeon).nextSibling.nextSibling)
+    } else {
+      ui.create(petIconCross({ img: pet.icon }), document.getElementById("tutorial-" + pet.dungeon).nextSibling.nextSibling)
+    }
+  })
+}
+
+
+export function setup(ctx) {
+  ctx.onInterfaceReady(async (ctx) => {
+    setTimeout(() => generatePetList(), 1000);
+  });
+}
